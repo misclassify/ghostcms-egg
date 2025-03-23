25576c4334a926e52d84a9faee10df6f5e889ffd
misclassify <misclassifyy@gmail.com>
2025-03-23 09:40:09 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,7 @@ sleep 1
 cd /home/container
 
 # Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+MODIFIED_STARTUP=$(eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+echo "Executing: ${MODIFIED_STARTUP}"
 
-# Run the Server
-${MODIFIED_STARTUP}
+exec ${MODIFIED_STARTUP}
