e40cac88d06e29736be6681d7cc39562c57fd8c2
misclassify <misclassifyy@gmail.com>
2025-03-23 09:39:04 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,5 @@
 #!/bin/ash
-
-# echo "Starting PHP-FPM..."
-# /usr/sbin/php-fpm7 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
-
 echo "Starting Ghost..."
-cd ./ghost && ghost start &
+cd ./ghost && ghost run &
+echo "Starting Caddy..."
 ./caddy-server run --watch --config ./caddy/Caddyfile
