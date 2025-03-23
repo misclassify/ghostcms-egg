486f69035d2f8f2be9277cce03b30cb8b7307f31
misclassify <misclassifyy@gmail.com>
2025-03-23 09:39:35 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/bin/ash
+set -e
 
 cd /mnt/server
 
-apk --no-cache add sudo curl
-apk add --no-cache 'su-exec>=0.2'
+apk add --no-cache curl sudo nodejs npm su-exec
 
-npm i --no-audit ghost-cli@latest -g
+npm install ghost-cli@latest -g
 
-mkdir /.npm
+mkdir -p /.npm
 chmod -R 755 /.npm
 
+# Copy necessary files
 cp -r ./temp/caddy /mnt/server/
 cp ./temp/start.sh /mnt/server
 curl "https://caddyserver.com/api/download?os=linux&arch=amd64&idempotency=33572405766393" -s --output /mnt/server/caddy-server
@@ -17,28 +18,22 @@ curl "https://caddyserver.com/api/download?os=linux&arch=amd64&idempotency=33572
 chmod +x /mnt/server/caddy-server
 chmod +x /mnt/server/start.sh
 
-mkdir /mnt/server/.ghost
-# route ghost config location to mount
+mkdir -p /mnt/server/.ghost
 ln -s /mnt/server/.ghost /.ghost
+
 chown -R nobody: /mnt/server/
 chmod -R u+w /mnt/server/
-# route yarn install location
-mkdir -p /.cache/yarn
-chown -R nobody: /.cache/yarn/
-chmod -R u+w /.cache/yarn/
 
 mkdir -p /home/container
 chown -R nobody: /home/container/
 chmod -R u+w /home/container/
-su -s /bin/ash "nobody" -c "ghost install local --no-start --no-enable --no-prompt --dir /home/container/ghost --process local"
+
+mkdir -p /.cache/yarn
+chown -R nobody: /.cache/yarn/
+chmod -R u+w /.cache/yarn/
+
+# Install Ghost headlessly with provided DB credentials
+su -s /bin/ash nobody -c "ghost install --no-start --no-enable --no-prompt --dir /home/container/ghost --db mysql --dbhost $1 --dbuser $2 --dbpass $3 --dbname $4"
 unlink /.ghost
 
 mv /home/container/ghost /mnt/server
-
-# ghostlink=$(readlink /mnt/server/ghost/current)
-# if [[ "${ghostlink:0:12}" == "/mnt/server/" ]]; then
-#     ln -sfn /home/container/${ghostlink:12} /mnt/server/ghost/current
-# fi
-
-# sed 's/\/mnt\/server\//\/home\/container\//g' /mnt/server/ghost/config.development.json > temp.json
-# mv temp.json /mnt/server/ghost/config.development.json
